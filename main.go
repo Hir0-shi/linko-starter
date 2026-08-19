eaeb076acf403fcf512772afe2ec9a27e5b6174e
Hir0-shi <62444653+Hir0-shi@users.noreply.github.com>
2026-08-19 03:25:21 +0200

    global logger vs. dependency injection

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"boot.dev/linko/internal/store"
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
-
-var logger = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -26,28 +25,38 @@ func main() {
 }
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	st, err := store.New(dataDir)
+	accessFile, err := os.Create("linko.access.log")
 	if err != nil {
-		logger.Printf("failed to create store: %v", err)
+		fmt.Fprintf(os.Stderr, "failed to create access log: %v\n", err)
 		return 1
 	}
-	s := newServer(*st, httpPort, cancel)
+	defer accessFile.Close()
+
+	accessLogger := log.New(accessFile, "INFO: ", log.LstdFlags)
+	stdLogger := log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
+
+	st, err := store.New(dataDir, stdLogger)
+	if err != nil {
+		stdLogger.Printf("failed to create store: %v", err)
+		return 1
+	}
+	s := newServer(*st, httpPort, cancel, accessLogger)
 	var serverErr error
 	go func() {
 		serverErr = s.start()
 	}()
 
 	<-ctx.Done()
-	logger.Println("Linko is shutting down")
+	stdLogger.Println("Linko is shutting down")
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		logger.Printf("failed to shutdown server: %v", err)
+		stdLogger.Printf("failed to shutdown server: %v", err)
 		return 1
 	}
 	if serverErr != nil {
-		logger.Printf("server error: %v", serverErr)
+		stdLogger.Printf("server error: %v", serverErr)
 		return 1
 	}
 	return 0
