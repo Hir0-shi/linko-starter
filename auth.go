07711bb9118be490fed477c93b938ad60a8a2052
Hir0-shi <62444653+Hir0-shi@users.noreply.github.com>
2026-08-22 17:33:26 +0200

    redundant logs add proper error label

--- auth.go
+++ auth.go
@@ -32,7 +32,7 @@ func (s *server) authMiddleware(next http.Handler) http.Handler {
 		}
 		ok, err := s.validatePassword(password, stored)
 		if err != nil {
-			s.logger.Info("error validating password", "user", username, "error", err)
+			s.logger.Error("error validating password", "user", username, "error", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
